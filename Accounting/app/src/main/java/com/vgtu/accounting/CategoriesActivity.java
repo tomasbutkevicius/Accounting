3e922e0ca1a3922f886ab20c7dd04cd77787d137
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-12-05 18:09:43 +0200

    display category list with image title description

--- Accounting/app/src/main/java/com/vgtu/accounting/CategoriesActivity.java
+++ Accounting/app/src/main/java/com/vgtu/accounting/CategoriesActivity.java
@@ -1,12 +1,20 @@
 package com.vgtu.accounting;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.vgtu.accounting.response.CategoryResponse;
@@ -29,7 +37,6 @@ public class CategoriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories_activity);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
-//        getSupportActionBar().setTitle("My categories");
         setUser();
         setCategoryList();
     }
@@ -52,14 +59,22 @@ public class CategoriesActivity extends AppCompatActivity {
                 if(response.isSuccessful()){
                     categories = response.body();
                     listView = findViewById(R.id.listview);
-                    ArrayList<String> arrayList = new ArrayList<>();
+                    ArrayList<String> titles = new ArrayList<>();
+                    ArrayList<String> descriptions = new ArrayList<>();
+                    ArrayList<Boolean> isParent = new ArrayList<>();
 
-                    for(CategoryResponse category: categories)
-                        arrayList.add(category.getTitle());
+                    for(CategoryResponse category: categories){
+                        titles.add(category.getTitle());
+                        descriptions.add(category.getDescription());
+                        if(category.getParentCategoryID() != 0){
+                            isParent.add(false);
+                        } else
+                            isParent.add(true);
+                    }
 
-                    ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.row, arrayList);
+                    MyAdapter adapter = new MyAdapter(getApplicationContext(), titles, descriptions, isParent);
 
-                    listView.setAdapter(arrayAdapter);
+                    listView.setAdapter(adapter);
 
                 } else {
                     String message = "Error occurred";
@@ -75,6 +90,38 @@ public class CategoriesActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        ArrayList<String> rTitle;
+        ArrayList<String> rDescription;
+        ArrayList<Boolean> isParent;
+
+        MyAdapter(Context c, ArrayList<String> titles, ArrayList<String> descriptions, ArrayList<Boolean> isParent){
+            super( c,  R.layout.category_row,  R.id.title,  titles);
+            this.context = c;
+            this.rTitle = titles;
+            this.rDescription = descriptions;
+            this.isParent = isParent;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.category_row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.title);
+            TextView myDescription = row.findViewById(R.id.subtitle);
+
+            if(!isParent.get(position))
+                images.setImageResource(0);
+            myTitle.setText(rTitle.get(position));
+            myDescription.setText(rDescription.get(position));
+
+            return row;
+        }
     }
 
     @Override
